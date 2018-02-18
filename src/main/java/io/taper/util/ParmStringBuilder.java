9657806063e45399dc37d665b5f48476d8d07337
ScottHensen <hensen.scott@gmail.com>
2018-02-17 17:34:24 -0700

    Initial Commit - extract works; write truncates string to 64k.

---
+++ src/main/java/io/taper/util/ParmStringBuilder.java
@@ -0,0 +1,26 @@
+package io.taper.util;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Map;
+
+public class ParmStringBuilder {
+
+	public static String getParmString(Map<String, String> parms)
+		throws UnsupportedEncodingException {
+		
+		StringBuilder result = new StringBuilder();
+		
+		for (Map.Entry<String, String> entry : parms.entrySet()) {
+			result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+			result.append("=");
+			result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+			result.append("&");
+		}
+		
+		String resultString = result.toString();
+		return resultString.length() > 0
+				? resultString.substring(0, resultString.length() - 1)
+				: resultString;
+	}
+}
