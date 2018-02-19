2ee4174c86517751b9c2b6bfe7c1d536e38bd874
ScottHensen <hensen.scott@gmail.com>
2018-02-19 12:56:55 -0700

    added jacoco to gradle; added test script

--- src/test/java/io/taper/util/HttpHelperTest.java
+++ src/test/java/io/taper/util/HttpHelperTest.java
@@ -2,18 +2,38 @@ package io.taper.util;
 
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+//import org.apache.commons.io.IOUtils; 
 
 public class HttpHelperTest {
 
 	@Test(expected = IOException.class)
-	public void testException() throws IOException {
+	public void givenUrlDoesNotExist_whenHttpGet_thenExceptionThrown() throws IOException {
 		// Given
 		URL url = new URL("https://this.is.an.unknown.url.io");
 		// When
 		InputStream response = HttpHelper.getStream(url);
+	}
+	
+	@Test
+	public void givenUrlExists_whenHttpGet_thenSuccess() throws IOException {
+		// Given
+		URL url = new URL("https://www.google.com");
+		// When
+		InputStream response = HttpHelper.getStream(url);
+
+		@SuppressWarnings("resource")
+		Scanner s = new Scanner(response).useDelimiter("\\A");
+		String responseString = s.hasNext() ? s.next() : "";
+		// Then
+		assertThat(responseString).containsIgnoringCase("Search");
 	}
 
 }
